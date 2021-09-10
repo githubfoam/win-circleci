e2bcccc5cd5df7c866d844332b1ecba35a14941e
githubfoam <lequestionis@gmail.com>
2021-09-11 01:56:38 +0300

    terminal is not fully functional

--- scripts/deploy-libvirt.sh
+++ scripts/deploy-libvirt.sh
@@ -8,9 +8,13 @@ set -o xtrace
 echo "=============================Install kvm qemu libvirt============================================================="
 apt-get -qq update
 apt-get install -y cpu-checker bridge-utils dnsmasq-base ebtables libvirt-bin libvirt-dev qemu-kvm qemu-utils ruby-dev
+
 systemctl status libvirtd
+set TERM=xterm 
 libvirtd --version
+
 egrep -c '(vmx|svm)' /proc/cpuinfo #If 0 it means that your CPU doesn't support hardware virtualization.If 1 or more it does - but you still need to make sure that virtualization is enabled in the BIOS.
 addgroup libvirtd
 adduser  $(id -un) libvirtd #ensure that your username is added to the group libvirtd
+
 kvm-ok
