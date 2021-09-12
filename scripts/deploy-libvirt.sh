39c3b208414e05aae7835541f41bae78cfd366da
githubfoam <lequestionis@gmail.com>
2021-09-12 23:26:25 +0300

    addgroup

--- scripts/deploy-libvirt.sh
+++ scripts/deploy-libvirt.sh
@@ -24,10 +24,10 @@ libvirtd --version
 
 #If 0 it means that your CPU doesn't support hardware virtualization.
 # If 1 or more it does - but you still need to make sure that virtualization is enabled in the BIOS.
-egrep -c '(vmx|svm)' /proc/cpuinfo 
+egrep -c '(vmx|svm)' /proc/cpuinfo #0
 
 
 addgroup libvirtd
 adduser  $(id -un) libvirtd #ensure that your username is added to the group libvirtd
 
-kvm-ok
+# kvm-ok
